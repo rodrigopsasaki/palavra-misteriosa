21c9a7ec650d30cfbbab6e148e404281901101ea
Rodrigo Sasaki <rodrigopsasaki@gmail.com>
2016-06-10 12:16:22 -0400

    relevant comments on jquery functions

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+  // go to next input when the current one is filled
   $(".letra").keyup(function() {
     $(this).val($(this).val().toUpperCase());
     var limit  = parseInt($(this).attr('maxlength'));
@@ -8,6 +10,7 @@ $(document).ready(function(){
     }
   });
 
+  // go back to the previous input if you hit backspace on an empty input
   $(".letra").keydown(function(e){
     if ((e.which == 8 || e.which == 46) && $(this).val() =='') {
       moveToPreviousInput(this);
